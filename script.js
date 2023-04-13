f22b05a05a1506b42039272359f805aade1b43a5
Saba Komladze <121091520+SabaKomladze@users.noreply.github.com>
2023-04-13 11:13:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const countDownDate = new Date("May 14, 2025 00:00:00").getTime();
+
+
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance/ ( 1000 * 60 * 60 * 24 ));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) /(1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) /1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+      }
+
+},1000);
